466c514e330897ab535db020925b31b2ef5d2aa3
Haruki Ambai <s1270128@u-aizu.ac.jp>
2023-02-25 19:18:10 +0900

    # feat : the first setting fot webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const prod = process.env.NODE_ENV === "production";
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: prod ? "production" : "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: __dirname + "/dist/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: [".ts", ".tsx", ".js", ".json"],
+        },
+        use: "ts-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  devtool: prod ? undefined : "source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./dist/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
