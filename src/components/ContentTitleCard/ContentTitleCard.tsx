9652a0b003b8631f4d38380bde60f1bfec8a5799
Haruki Ambai <s1270128@u-aizu.ac.jp>
2023-06-30 19:29:20 +0900

    # feat : title content done

--- src/components/ContentTitleCard/ContentTitleCard.tsx
+++ src/components/ContentTitleCard/ContentTitleCard.tsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import { Box } from '@mui/system'
+import { BiTimeFive } from 'react-icons/bi'
+import { FaUserAlt } from 'react-icons/fa'
 import './ContentTitleCard.scss'
 
 export const ContentTitleCard = () => {
   return (
     <Box className='title-content-wrapper'>
       <Box className='title-content'>
-        <h1 className='title-icon'>😀</h1>
-        <div className='title-detail'></div>
+        <h1>😀</h1>
+        <div className='title-detail'>
+          <h1>Here is the title shoud be text align center</h1>
+          <div className='time-and-user'>
+            <div className='to-read'>
+              <BiTimeFive />
+              <p>10 minutes</p>
+            </div>
+            <div className='to-read'>
+              <FaUserAlt />
+              <p>Haruki Ambai</p>
+            </div>
+          </div>
+        </div>
       </Box>
     </Box>
   )
