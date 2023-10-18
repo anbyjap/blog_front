82d26e1867d077a99561964c74845e4cd6dc79db
Anbyjap <haruru898@gmail.com>
2023-10-18 00:14:00 -0700

    # revert

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import svgr from 'vite-plugin-svgr'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [react(), svgr()],
-})
+import Svgr from 'vite-plugin-svgr'
+
+export default {
+  plugins: [react(), Svgr()],
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // Add global SCSS variables here if needed
+      },
+    },
+  },
+
+  esbuild: {
+    jsxInject: 'import React from "react"',
+  },
+
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    // ... other constants
+  },
+
+  // Additional configuration can go here...
+}
