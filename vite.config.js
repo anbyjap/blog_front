85644d4f7e2de1a739d7ad72cc8eda78e4442f08
Anbyjap <haruru898@gmail.com>
2023-10-18 23:55:06 -0700

    # change :

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,4 @@ import svgr from '@svgr/rollup'
 export default defineConfig({
   base: './',
   plugins: [react(), svgr()],
-  build: {
-    rollupOptions: {
-      external: ['/src/images/yenn.svg'],
-    },
-  },
 })
