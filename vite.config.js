c8bf33295a1f01001dfaccb7e78749ac02c2d20a
Anbyjap <haruru898@gmail.com>
2023-10-18 23:59:28 -0700

    a

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import svgr from '@svgr/rollup'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
